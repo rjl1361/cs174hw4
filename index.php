927732ecc229c8242f8d82108db032b54bee3df8
misha <misha.behey@gmail.com>
2017-04-18 14:24:39 -0700

    project setup based on the assignment requirements

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web Sheet Application</title>
+</head>
+<body>
+
+</body>
+
+<a href="">Web Sheets</a>
+
+<form method="get" id="websheet_landing_page">
+    <input type="text" name="Enter file name"/>
+    <button type="Submit" onclick="" value="Submit">Go</button>
+</form>
+
+
+<script type="text/javascript" ref="scripts/spreadsheet.js">
+</script>
+</html>
