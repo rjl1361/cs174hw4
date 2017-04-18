1db37bc58affca3e7e8ee19acbf7048ce846e6b0
misha <misha.behey@gmail.com>
2017-04-18 14:39:10 -0700

    got rid off HTML code in index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Web Sheet Application</title>
-</head>
-<body>
-
-</body>
-
-<a href="">Web Sheets</a>
-
-<form method="get" id="websheet_landing_page">
-    <input type="text" name="Enter file name"/>
-    <button type="Submit" onclick="" value="Submit">Go</button>
-</form>
+<?php
 
 
-<script type="text/javascript" ref="scripts/spreadsheet.js">
-</script>
-</html>
+
