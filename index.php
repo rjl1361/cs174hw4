5bb2ddf375e6822848bb1f543b3b9b0ca9ce1a0c
misha <misha.behey@gmail.com>
2017-04-20 14:31:28 -0700

    require to create an autoload

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 
 
-
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+use HMRTeam\hw4\controllers as Contoller;
+
+
+//in here we would add a controller once we get the solution 3 back
+//nothing else goes on index.php page but controller
