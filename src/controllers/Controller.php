d59de0e93e850834f8a61575b77224a0c1c6ced2
misha <misha.behey@gmail.com>
2017-04-20 15:39:08 -0700

    controller change

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -14,4 +14,12 @@ namespace HMRTeam\hw4\controllers;
 
 class Controller {
     //handle views in here
+    protected $model;
+    protected $view;
+
+    public function __construct()
+    {
+        $this->model = 0;
+        $this->view = 0;
+    }
 }
