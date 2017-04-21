1926afa91a8b89d5ad9814f8df09d7bf5d29e0b2
misha <misha.behey@gmail.com>
2017-04-20 23:02:57 -0700

    updated config file

---
+++ src/configs/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author Richard Lack
+ * @author Hien Nguyen
+ * @author Mykhailo Behei
+ */
+
+namespace HMRTeam\hw4;
+class Config {
+    const user ='root';
+    const password = 'root';
+    const host = '127.0.0.1';
+    const port = 8080;
+    const db = 'spreadsheet';
+}
