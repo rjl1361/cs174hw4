888babfbac6a004e1e4afffb6b04e2e83824f60e
misha <misha.behey@gmail.com>
2017-04-20 23:59:54 -0700

    adder header that implements layout

---
+++ src/views/helpers/Header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Mykhailo Behei
+ * @author Hien Nguyen
+ * @author Richard Lack
+ */
+
+require_once('Layout.php');
+
+class Header implements Layout {
+
+
+    function render($data)
+    {
+        // TODO: Implement render() method.
+    }
+}
