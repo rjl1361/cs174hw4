3248bc49d1b9354fc6f425fb02c8dd556c691501
misha <misha.behey@gmail.com>
2017-04-23 23:47:41 -0700

    started adding the database

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -5,3 +5,16 @@
  * Date: 4/18/17
  * Time: 2:34 PM
  */
+namespace HMRTeam\hw4;
+require_once ("Config.php");
+
+$hostname = \HMRTeam\hw4\Config::host.":".\HMRTeam\hw4\Config::port;
+
+$db = new \mysqli($hostname, \HMRTeam\hw4\Config::user, \HMRTeam\hw4\Config::password);
+
+if ($db -> connect_error)
+{
+    die('Could not connect to the database');
+}
+
+echo "successfully connection\n";
