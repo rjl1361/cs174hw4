2421759ca28fc1493d568361e80142318872a056
misha <misha.behey@gmail.com>
2017-04-24 09:54:43 -0700

    added xhttp request

--- src/js/spreadsheet.js
+++ src/js/spreadsheet.js
@@ -255,6 +255,8 @@ function Spreadsheet(spreadsheet_id, supplied_data)
         var column = target.cellIndex - 1;
         var length = data.length;
         var width = data[0].length;
+        var xhttp = new XMLHttpRequest(); //that was easy
+        
         if (row >= 0 && column >= 0) {
             var new_value = prompt(self.letterRepresentation(column) +
                 (row + 1), data[row][column]);
