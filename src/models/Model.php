e054d2a11de191ef5f83e3c94f2fc9758aaa550a
misha <misha.behey@gmail.com>
2017-04-24 10:15:16 -0700

    a model should be done, I hope

--- src/models/Model.php
+++ src/models/Model.php
@@ -5,3 +5,30 @@
  * Date: 4/20/17
  * Time: 2:19 AM
  */
+
+use \HMRTeam\hw4\configs as C;
+
+class Model
+{
+    public $user;
+    public $password;
+    public $port;
+
+    public function __construct()
+    {
+        $configs = new C\Config;
+        $this->user = $configs->user;
+        $this->password = $configs->password;
+        $this->port = $configs->port;
+    }
+
+    public function connect()
+    {
+        return new \mysqli("127.0.0.1:" . $this->port, $this->user, $this->password);
+    }
+
+    public function connectTO ($db)
+    {
+        return new\mysqli("127.0.0.1:" . $this->port, $this->user, $this->password, $db);
+    }
+}
