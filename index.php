4617b502c81e2f903454fcec0d35abdefde1143c
rjl1361 <rjl136@live.com>
2017-04-25 14:19:25 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
@@ -8,3 +6,18 @@ use HMRTeam\hw4\controllers as Contoller;
 
 //in here we would add a controller once we get the solution 3 back
 //nothing else goes on index.php page but controller
+if(!isset($_REQUEST['c']))
+{
+	header("Location: index.php?c=MainController");
+}
+else{
+
+
+}
+
+
+//checks if there is a new list to be saved into the database
+if (isset($_POST['goButton']))
+{
+	
+}
